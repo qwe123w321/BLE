bbcca24835e04edeb0fbdf019e1f163bd025e5a6
qwe123w321 <59343771+qwe123w321@users.noreply.github.com>
2023-03-24 15:38:01 +0800

    TEST

--- app/src/main/java/com/example/pelvicfloormuscletraining/PreviewActivity.java
+++ app/src/main/java/com/example/pelvicfloormuscletraining/PreviewActivity.java
@@ -1,19 +1,53 @@
 package com.example.pelvicfloormuscletraining;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import android.Manifest;
+
+import org.jetbrains.annotations.NotNull;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+//import okhttp3.RequestBody;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import com.opencsv.CSVWriter;
+import okhttp3.Callback;
 
 public class PreviewActivity extends AppCompatActivity {
     private LinearLayout answersLayout;
@@ -21,7 +55,8 @@ public class PreviewActivity extends AppCompatActivity {
     private List<Questionnaire.Question> questions;
     private Button backButton;
     private Button finishButton;
-    private ViewPager viewPager;
+    private static final int REQUEST_PERMISSIONS_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +87,7 @@ public class PreviewActivity extends AppCompatActivity {
                 if (unansweredQuestionIndex != -1) {
                     // 還有未回答的問題，滾動到第一個未回答的問題
                     Toast.makeText(PreviewActivity.this, "有未回答的問題，請繼續填寫", Toast.LENGTH_SHORT).show();
-                    viewPager.setCurrentItem(unansweredQuestionIndex, true);
+                    //viewPager.setCurrentItem(unansweredQuestionIndex, true);
                 } else {
                     // 所有問題都已回答，儲存答案
                     Toast.makeText(PreviewActivity.this, "答案已保存", Toast.LENGTH_SHORT).show();
@@ -63,23 +98,157 @@ public class PreviewActivity extends AppCompatActivity {
         // 獲取問題列表
         //questions = ((Questionnaire) getApplicationContext()).getQuestions();
         questions = (List<Questionnaire.Question>) getIntent().getSerializableExtra("questions");
-
+        Log.e("PreviewActivity", "onCreate: questions size: " + questions.size());
         for (int i = 0; i < questions.size(); i++) {
-            String answer = sharedPreferences.getString("question_" + i, null);
+
+            Questionnaire.Question question = questions.get(i);
 
             TextView questionView = new TextView(this);
-            questionView.setText(questions.get(i).questionText);
-            answersLayout.addView(questionView);
+            questionView.setText(question.questionText);
+            questionView.setTextSize(30);
 
             TextView answerView = new TextView(this);
-            if (answer != null) {
+            String answer = sharedPreferences.getString("question_" + i, null);
+            Log.e("Answer", Integer.toString(i)+" : " +answer+":" + answer.isEmpty());
+            if (!answer.isEmpty()) {
                 answerView.setText(answer);
+                answerView.setTextSize(30);
+
+                int selectedAnswerIndex = -1;
+                for (int j = 0; j < question.options.size(); j++) {
+                    if (answer.equals(question.options.get(j))) {
+                        selectedAnswerIndex = j;
+                        break;
+                    }
+                }
+
+                if (selectedAnswerIndex >= 0 && selectedAnswerIndex < question.optionImages.size()) {
+                    int imageResource = question.optionImages.get(selectedAnswerIndex);
+                    Drawable drawable = ContextCompat.getDrawable(this, imageResource);
+                    if (drawable != null) {
+                        drawable.setBounds(0, 0, 100, 100); // 设置图片大小，例如 100x100 像素
+                        answerView.setCompoundDrawables(drawable, null, null, null); // 在选项文字前面添加图片
+                    }
+                    answerView.setCompoundDrawablePadding(16); // 设置图片和文字之间的边距
+                }
+                answersLayout.addView(questionView);
+                answersLayout.addView(answerView);
             } else {
-                answerView.setText("尚未做答，請回去填寫");
-                answerView.setBackground(ContextCompat.getDrawable(this, R.drawable.red_border)); // 紅色邊框背景
+                LinearLayout questionAnswerLayout = new LinearLayout(this);
+                questionAnswerLayout.setOrientation(LinearLayout.VERTICAL);
+                answerView.setText("尚未做答，請返回填寫");
+                answerView.setTextSize(30);
+                questionAnswerLayout.addView(questionView);
+                questionAnswerLayout.addView(answerView);
+                questionAnswerLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.red_border)); //
+                answersLayout.addView(questionAnswerLayout);
             }
-            answersLayout.addView(answerView);
+        }
+    }
+    private void exportAndUploadCSV() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS_CODE);
+        } else {
+            Map<String, String> answersMap = loadAnswersFromSharedPreferences();
+            File csvFile = createCSVFile(answersMap);
+            Log.d("CSV File Path", csvFile.getAbsolutePath());
+            if (csvFile != null) {
+                // 上傳文件
+                uploadFile(csvFile);
+            }
+        }
+    }
+
+    private Map<String, String> loadAnswersFromSharedPreferences() {
+        Map<String, String> answersMap = new HashMap<>();
+        SharedPreferences sharedPreferences = getSharedPreferences("answers", MODE_PRIVATE);
+
+        for (String key : sharedPreferences.getAll().keySet()) {
+            answersMap.put(key, sharedPreferences.getString(key, ""));
+        }
+
+        return answersMap;
+    }
+    private File createCSVFile(Map<String, String> answersMap) {
+        File csvFile = null;
+        try {
+            // 創建文件名
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+            String fileName = "answers_" + dateFormat.format(new Date()) + ".csv";
+
+            // 在應用的內部存儲區域創建文件
+            csvFile = new File(getFilesDir(), fileName);
+
+            // 將數據寫入CSV文件
+            FileWriter fileWriter = new FileWriter(csvFile);
+            CSVWriter csvWriter = new CSVWriter(fileWriter);
+
+            for (Map.Entry<String, String> entry : answersMap.entrySet()) {
+                String[] data = {entry.getKey(), entry.getValue()};
+                csvWriter.writeNext(data);
+            }
+
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return csvFile;
+    }
+
+    private void uploadFile(File file) {
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://163.13.127.92/upload.php";
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("uploaded_file", file.getName(),
+                        RequestBody.create(MediaType.parse("text/csv"), file))
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+
+            @Override
+            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
+                Log.e("Upload", "Error uploading file: " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NotNull okhttp3.Call call, @NotNull okhttp3.Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("Upload", "File successfully uploaded");
+                } else {
+                    Log.e("Upload", "Error uploading file: " + response.message());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                exportAndUploadCSV();
+            } else {
+                Log.e("onRequestPermissionsResult", "Permissions not granted");
+                Toast.makeText(this, "存儲和讀取權限被拒絕，無法將資料保存為CSV文件。", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
 }
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/pelvicfloormuscletraining/Questionnaire.java
+++ app/src/main/java/com/example/pelvicfloormuscletraining/Questionnaire.java
@@ -9,9 +9,11 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -52,13 +54,34 @@ public class Questionnaire extends AppCompatActivity implements Serializable{
         questions = new ArrayList<>();
         List<Integer> optionImages1 = Arrays.asList(R.drawable.option_1, R.drawable.option_2, R.drawable.option_3, R.drawable.option_5);
         List<Integer> optionImages2 = Arrays.asList(R.drawable.option_1, R.drawable.option_2, R.drawable.option_3, R.drawable.option_4, R.drawable.option_5);
+        List<Integer> optionImages3 = Arrays.asList(R.drawable.none,R.drawable.option_1, R.drawable.option_2, R.drawable.option_3, R.drawable.option_4, R.drawable.option_5);
+
         // 前两个问题有4个选项
-        questions.add(new Question("整體來說，您如何評價您的生活品質", Arrays.asList("选项1", "选项2", "选项3", "选项4"),optionImages1));
-        questions.add(new Question("问题2", Arrays.asList("选项1", "选项2", "选项3", "选项4"),optionImages1));
+        questions.add(new Question("1.是否感到下腹部有壓迫感", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("2.是否感到下腹部有沉重感及悶痛感", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("3.是否感到有東西從會陰部膨出或掉出", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("4.是否需要推動會陰部及肛門周圍才能順利排便", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("5.是否常常有解尿解不乾淨的問題", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("6.是否需要用手指將會陰部膨出的部分推回才能順利解尿", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("7.是否需要常常上廁所小便", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("8.尿急時，是否會來不及到廁所就尿出來", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("9.活動或用力時(咳嗽、打噴嚏、大笑)是否會漏尿", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("10.尿液會自然流出來", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("11.排尿困難：", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("12.下腹或生殖器疼痛感", Arrays.asList("不怎麼困擾", "有點困擾", "稍微困擾", "非常困擾"),optionImages1));
+        questions.add(new Question("13.膀胱問題是否影響你做家事(打掃、洗衣服)", Arrays.asList("沒有影響", "有點影響", "稍微影響", "嚴重影響"),optionImages1));
+        questions.add(new Question("14.膀胱問題是否影響你運動(散步、爬山、跳舞、游泳)", Arrays.asList("沒有影響", "有點影響", "稍微影響", "嚴重影響"),optionImages1));
+        questions.add(new Question("15.膀胱問題是否影響你外出休閒娛樂(唱歌、看電影、遊樂園)", Arrays.asList("沒有影響", "有點影響", "稍微影響", "嚴重影響"),optionImages1));
+        questions.add(new Question("16.膀胱問題是否影響你搭車或開車外出(離家超過30分鐘以上)", Arrays.asList("沒有影響", "有點影響", "稍微影響", "嚴重影響"),optionImages1));
+        questions.add(new Question("17.膀胱問題是否影響你社交活動(拜訪親友、婚喪喜慶)", Arrays.asList("沒有影響", "有點影響", "稍微影響", "嚴重影響"),optionImages1));
+        questions.add(new Question("18.膀胱問題是否影響你情緒(焦慮、憂慮、緊張、尷尬)", Arrays.asList("沒有影響", "有點影響", "稍微影響", "嚴重影響"),optionImages1));
+        questions.add(new Question("19.膀胱問題是否影響你挫折感(沮喪、鬱卒、受挫)", Arrays.asList("沒有影響", "有點影響", "稍微影響", "嚴重影響"),optionImages1));
 
         // 后两个问题有5个选项
-        questions.add(new Question("问题3", Arrays.asList("选项1", "选项2", "选项3", "选项4", "选项5"),optionImages2));
-        questions.add(new Question("问题4", Arrays.asList("选项1", "选项2", "选项3", "选项4", "选项5"),optionImages2));
+        questions.add(new Question("20.在不論何種性行為時，會有小便(或是大便)失禁的問題嗎?", Arrays.asList("從來沒有", "很少", "有時候", "常有", "都會有"),optionImages2));
+        questions.add(new Question("21.性交時會覺得疼痛嗎?(如果您沒有性交的行為，請勾選空格)", Arrays.asList("空格","從來沒有", "很少", "有時候", "常有", "都會有"),optionImages3));
+        questions.add(new Question("22.有多害怕會尿失禁或大便失禁或陰道膨出(無論是膀胱、直腸或陰道脫垂)，而使您避免性生活?", Arrays.asList("完全不會", "一點點", "有些", "非常會"),optionImages1));
+        questions.add(new Question("23.請圈選(依程度1~5個分級)最能代表你對性生活的感覺", Arrays.asList("滿意5分", "4  分", "3  分", "2  分", "不滿意1分"),optionImages2));
 
     }
 
@@ -71,14 +94,22 @@ public class Questionnaire extends AppCompatActivity implements Serializable{
         nextButton = findViewById(R.id.next_button);
         questionText = findViewById(R.id.question_text);
         optionGroup = findViewById(R.id.option_group);
-        sharedPreferences = getSharedPreferences("answers", Context.MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("answers", MODE_PRIVATE);
         initQuestions();
         currentQuestionIndex = 0;
+        Log.e("SIZE",Integer.toString(questions.size()));
+        for(int i=0;i<questions.size();i++){
+            Log.e("TAG", Integer.toString(i));
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("question_" + Integer.toString(i), "");
+            editor.apply();
+        }
         updateUI();
 
         previousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveAnswer();
                 if (currentQuestionIndex > 0) {
                     currentQuestionIndex--;
                     updateUI();
@@ -89,9 +120,11 @@ public class Questionnaire extends AppCompatActivity implements Serializable{
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveAnswer();
                 if (currentQuestionIndex < questions.size() - 1) {
                     currentQuestionIndex++;
                     updateUI();
+
                 } else {
                     // 跳转到答案预览页面
 //                    Intent intent = new Intent(Questionnaire.this, PreviewActivity.class);
@@ -118,7 +151,7 @@ public class Questionnaire extends AppCompatActivity implements Serializable{
         radioButton.setTextSize(30);
         Drawable drawable = ContextCompat.getDrawable(this, imageResource);
         if (drawable != null) {
-            drawable.setBounds(0, 0, 200, 200); // 设置图片大小，例如 100x100 像素
+            drawable.setBounds(0, 0, 150, 150); // 设置图片大小，例如 100x100 像素
             radioButton.setCompoundDrawables(drawable, null, null, null); // 在选项文字前面添加图片
         }
         radioButton.setCompoundDrawablePadding(16); // 添加这一行来设置图片和文字之间的边距
@@ -129,35 +162,177 @@ public class Questionnaire extends AppCompatActivity implements Serializable{
     private void showQuestion(Question question) {
         questionText.setText(question.questionText);
         optionGroup.removeAllViews();
-
+        Log.e("showQuestion", "showQuestion: ");
         for (int i = 0; i < question.options.size(); i++) {
             RadioButton radioButton = createOptionRadioButton(question.options.get(i), question.optionImages.get(i));
             optionGroup.addView(radioButton);
 
             if (i < question.options.size() - 1) { // 不在最后一个选项后添加分隔线
+                FrameLayout separatorLayout = new FrameLayout(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1);
+                layoutParams.setMargins(0, 16, 0, 16);
+                separatorLayout.setLayoutParams(layoutParams);
+
                 View separator = new View(this); // 创建一个新的 View 作为分隔线
-                separator.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1));
+                separator.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1));
                 separator.setBackgroundColor(Color.DKGRAY); // 设置分隔线的颜色
-                optionGroup.addView(separator); // 将分隔线添加到选项组中
+                separatorLayout.addView(separator); // 将分隔线添加到布局中
+
+                optionGroup.addView(separatorLayout); // 将分隔线布局添加到选项组中
             }
         }
-
+        Log.e("ANSWER", Integer.toString(question.answer));
         if (question.answer != -1) {
+            Log.e("ANSWER", Integer.toString(question.answer));
             ((RadioButton) optionGroup.getChildAt(question.answer)).setChecked(true);
+            //((RadioButton) optionGroup.getChildAt(question.answer * 2)).setChecked(true);
         }
     }
 
     public List<Question> getQuestions() {
         return questions;
     }
-    private void updateUI() {
-        Question currentQuestion = questions.get(currentQuestionIndex);
+//    private void updateUI() {
+//        Question currentQuestion = questions.get(currentQuestionIndex);
+//
+//        // 调用 showQuestion() 方法以显示问题及其选项
+//        showQuestion(currentQuestion);
+//
+//        previousButton.setVisibility(currentQuestionIndex == 0 ? View.INVISIBLE : View.VISIBLE);
+//        nextButton.setText(currentQuestionIndex == questions.size() - 1 ? "預覽答案" : "下一題");
+//        optionGroup.setOnCheckedChangeListener(null);//(new RadioGroup.OnCheckedChangeListener() {
 
-        // 调用 showQuestion() 方法以显示问题及其选项
+
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                RadioButton checkedRadioButton = findViewById(checkedId);
+//                if (checkedRadioButton != null) {
+//                    String selectedAnswer = checkedRadioButton.getText().toString();
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putString("question_" + currentQuestionIndex, selectedAnswer);
+//                    editor.apply();
+//
+//                    // 更新当前问题的答案
+//                    currentQuestion.answer = optionGroup.indexOfChild(checkedRadioButton);
+//                }
+//            }
+//        });
+
+//        // 恢复选中的答案
+//        String savedAnswer = sharedPreferences.getString("question_" + currentQuestionIndex, null);
+//        if (savedAnswer != null) {
+//            for (int i = 0; i < optionGroup.getChildCount(); i++) {
+//                View child = optionGroup.getChildAt(i);
+//                if (child instanceof RadioButton) { // 添加此行以确保只处理 RadioButton
+//                    RadioButton radioButton = (RadioButton) child;
+//                    if (radioButton.getText().toString().equals(savedAnswer)) {
+//                        radioButton.setChecked(true);
+//                        break;
+//                    }
+//                }
+//            }
+//        } else {
+//            optionGroup.clearCheck();
+//        }
+//        optionGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                saveAnswer(); // 將saveAnswer()方法調用移至此處
+//                RadioButton checkedRadioButton = findViewById(checkedId);
+//                if (checkedRadioButton != null) {
+//                    String selectedAnswer = checkedRadioButton.getText().toString();
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putString("question_" + currentQuestionIndex, selectedAnswer);
+//                    editor.apply();
+//
+////                    // 更新当前问题的答案
+////                    currentQuestion.answer = optionGroup.indexOfChild(checkedRadioButton);
+//                    // 更新当前问题的答案
+//                    int index = optionGroup.indexOfChild(checkedRadioButton);
+//                    if (index % 2 == 0) {
+//                        currentQuestion.answer = index / 2;
+//                    }
+//                }
+//            }
+//        });
+//    }
+
+//        // 恢复选中的答案
+//        if (currentQuestion.answer != -1) {
+//            ((RadioButton) optionGroup.getChildAt(currentQuestion.answer * 2)).setChecked(true);
+//        } else {
+//            optionGroup.clearCheck();
+//        }
+//        optionGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                saveAnswer(); // 將saveAnswer()方法調用移至此處
+//                RadioButton checkedRadioButton = findViewById(checkedId);
+//                if (checkedRadioButton != null) {
+//                    String selectedAnswer = checkedRadioButton.getText().toString();
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putString("question_" + currentQuestionIndex, selectedAnswer);
+//                    editor.apply();
+
+//
+//                    // 更新当前问题的答案
+//                    int index = optionGroup.indexOfChild(checkedRadioButton);
+//                    if (index % 2 == 0) {
+//                        currentQuestion.answer = index / 2;
+//                    }
+
+//                }
+//            }
+//        });
+//    }
+    private void saveAnswer() {
+
+        int selectedId = optionGroup.getCheckedRadioButtonId();
+        if (selectedId != -1) {
+            RadioButton selectedRadioButton = findViewById(selectedId);
+            String answer = selectedRadioButton.getText().toString();
+
+            // 將答案存儲到 SharedPreferences
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("question_" + currentQuestionIndex, answer);
+            editor.apply();
+        }
+    }
+//}
+
+
+    private void updateUI() {
+
+        Question currentQuestion = questions.get(currentQuestionIndex);
+        // 暫時取消 optionGroup 的 OnCheckedChangeListener
+        optionGroup.setOnCheckedChangeListener(null);
         showQuestion(currentQuestion);
 
         previousButton.setVisibility(currentQuestionIndex == 0 ? View.INVISIBLE : View.VISIBLE);
-        nextButton.setText(currentQuestionIndex == questions.size() - 1 ? "预览答案" : "下一题");
+        nextButton.setText(currentQuestionIndex == questions.size() - 1 ? "預覽答案" : "下一題");
+
+
+
+//        // 恢复选中的答案
+//        String savedAnswer = sharedPreferences.getString("question_" + currentQuestionIndex, null);
+//        if (savedAnswer != null) {
+//            for (int i = 0; i < optionGroup.getChildCount(); i++) {
+//                RadioButton radioButton = (RadioButton) optionGroup.getChildAt(i);
+//                if (radioButton.getText().toString().equals(savedAnswer)) {
+//                    radioButton.setChecked(true);
+//                    break;
+//                }
+//            }
+//        } else {
+//            optionGroup.clearCheck();
+//        }
+        Log.e("currentQuestion", Integer.toString(currentQuestion.answer));
+                // 恢复选中的答案
+        if (currentQuestion.answer != -1) {
+            ((RadioButton) optionGroup.getChildAt(currentQuestion.answer)).setChecked(true);
+        } else {
+            optionGroup.clearCheck();
+        }
         optionGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -167,28 +342,13 @@ public class Questionnaire extends AppCompatActivity implements Serializable{
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.putString("question_" + currentQuestionIndex, selectedAnswer);
                     editor.apply();
-
-                    // 更新当前问题的答案
-                    currentQuestion.answer = optionGroup.indexOfChild(checkedRadioButton);
+                    // 找到选中选项在 optionGroup 中的位置
+                    int selectedIndex = optionGroup.indexOfChild(checkedRadioButton);
+                    // 更新 currentQuestion.answer
+                    currentQuestion.answer = selectedIndex;
+                    Log.e("onCheckedChanged", Integer.toString(currentQuestion.answer));
                 }
             }
         });
-
-        // 恢复选中的答案
-        String savedAnswer = sharedPreferences.getString("question_" + currentQuestionIndex, null);
-        if (savedAnswer != null) {
-            for (int i = 0; i < optionGroup.getChildCount(); i++) {
-                View child = optionGroup.getChildAt(i);
-                if (child instanceof RadioButton) { // 添加此行以确保只处理 RadioButton
-                    RadioButton radioButton = (RadioButton) child;
-                    if (radioButton.getText().toString().equals(savedAnswer)) {
-                        radioButton.setChecked(true);
-                        break;
-                    }
-                }
-            }
-        } else {
-            optionGroup.clearCheck();
-        }
     }
 }
