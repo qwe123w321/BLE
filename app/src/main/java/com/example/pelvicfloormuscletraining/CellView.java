dca4a6c101b0934734029c427597baf061474e15
qwe123w321 <59343771+qwe123w321@users.noreply.github.com>
2023-03-27 15:33:00 +0800

    TEST

---
+++ app/src/main/java/com/example/pelvicfloormuscletraining/CellView.java
@@ -0,0 +1,68 @@
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CellView extends View {
+    private int dayOfMonth;
+    private int starColor;
+    private boolean showStar;
+
+    public CellView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CellView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CellView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        dayOfMonth = 0;
+        starColor = Color.BLUE;
+        showStar = false;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (showStar) {
+            drawStar(canvas);
+        }
+
+        if (dayOfMonth > 0) {
+            drawDayOfMonth(canvas);
+        }
+    }
+
+    private void drawStar(Canvas canvas) {
+        // 繪製星星
+    }
+
+    private void drawDayOfMonth(Canvas canvas) {
+        // 繪製日期
+    }
+
+    public void setDayOfMonth(int dayOfMonth) {
+        this.dayOfMonth = dayOfMonth;
+        invalidate();
+    }
+
+    public void setShowStar(boolean showStar) {
+        this.showStar = showStar;
+        invalidate();
+    }
+
+    public void setStarColor(int starColor) {
+        this.starColor = starColor;
+        invalidate();
+    }
+}
--- app/src/main/java/com/example/pelvicfloormuscletraining/history.java
+++ app/src/main/java/com/example/pelvicfloormuscletraining/history.java
@@ -2,55 +2,122 @@ package com.example.pelvicfloormuscletraining;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.pelvicfloormuscletraining.CellView;
+import java.util.concurrent.TimeUnit;
+
 public class history extends AppCompatActivity {
-    private GridLayout gridLayout;
+    private LinearLayout gridContainer;
+    private int clickCount;
+    private int daysPassed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        gridLayout = findViewById(R.id.gridLayout);
-// 获取GridLayout对象
-        GridLayout gridLayout = findViewById(R.id.gridLayout);
 
-// 获取LayoutInflater对象
-        LayoutInflater inflater = LayoutInflater.from(this);
+        gridContainer = findViewById(R.id.grid_container);
+        clickCount = getIntent().getIntExtra("clickCount", 0);
+        daysPassed = (int) TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis() - getInstallTime());
 
-// 循环动态生成30个格子，并添加到GridLayout中
-        for (int i = 1; i <= 30; i++) {
-            // 加载一个包含一个TextView的布局文件
-            //View cellView = inflater.inflate(R.layout., gridLayout, false);
+        // 生成格子
+        generateGrid();
 
-            // 获取格子的TextView对象，并设置其文本
-//            TextView cellText = cellView.findViewById(R.id.cell_text);
-//            cellText.setText(String.valueOf(i));
-//
-//            // 将格子添加到GridLayout中
-//            gridLayout.addView(cellView);
+        // 更新格子內容
+        updateGridContent(2);
+    }
+
+    private void generateGrid() {
+        for (int i = 0; i < 6; i++) {
+            LinearLayout row = new LinearLayout(this);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+            row.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            for (int j = 0; j < 5; j++) {
+                View cell = createCell();
+                row.addView(cell);
+            }
+
+            gridContainer.addView(row);
         }
     }
-    public void onGridItemClick(View view) {
-        TextView textView = (TextView) view;
-        int index = Integer.parseInt(textView.getText().toString());
 
-        if (textView.isSelected()) {
-            textView.setSelected(false);
-            textView.setBackgroundColor(Color.parseColor("#cccccc"));
-            ImageView icon = (ImageView) gridLayout.findViewWithTag("icon_" + index);
-            icon.setVisibility(View.GONE);
-        } else {
-            textView.setSelected(true);
-            textView.setBackgroundColor(Color.BLUE);
-            ImageView icon = (ImageView) gridLayout.findViewWithTag("icon_" + index);
-            icon.setVisibility(View.VISIBLE);
+    private View createCell() {
+        View cell = new View(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, 100);
+        layoutParams.weight = 1;
+        layoutParams.setMargins(5, 5, 5, 5);
+        cell.setLayoutParams(layoutParams);
+        cell.setBackgroundResource(R.drawable.cell_background);
+        return cell;
+    }
+
+    private void updateGridContent(int selectedMonth) {
+        // 使用 selectedMonth 來確定要顯示的數據
+        int dayOfMonth = (daysPassed % 30) + 1;
+        int month = daysPassed / 30;
+
+        // 更新格子內容，例如顯示星星和數字
+        // 根據需求更新其他 UI 元素，例如自動選擇當前月份的按鈕
+        for (int i = 0; i < 6; i++) {
+            LinearLayout row = (LinearLayout) gridContainer.getChildAt(i);
+
+            for (int j = 0; j < 5; j++) {
+                int currentDay = i * 5 + j + 1;
+                CellView cell = (CellView) row.getChildAt(j);
+
+                if (currentDay <= dayOfMonth) {
+                    cell.setDayOfMonth(currentDay);
+
+                    if (currentDay == dayOfMonth) {
+                        cell.setBackgroundResource(R.drawable.cell_today_background); // 使用另一個 drawable 作為當天的背景
+                    }
+
+                    if (clickCount == 1 && currentDay == 1) {
+                        cell.setShowStar(true);
+                        cell.setStarColor(Color.BLUE);
+                    } else if (clickCount == 2 && currentDay == 2) {
+                        cell.setShowStar(true);
+                        cell.setStarColor(Color.YELLOW);
+                    } else if (clickCount == 3 && currentDay == 1 && month == 2) {
+                        cell.setShowStar(true);
+                        cell.setStarColor(Color.RED);
+                    }
+                }
+            }
         }
     }
+
+
+
+    private long getInstallTime() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            return packageInfo.firstInstallTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return System.currentTimeMillis();
+        }
+    }
+
+    private View.OnClickListener monthButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int selectedMonth = Integer.parseInt(((Button) v).getText().toString());
+            updateGridContentForMonth(selectedMonth);
+        }
+    };
 }
